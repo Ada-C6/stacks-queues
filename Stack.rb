a685fbf820ff5da50e086d53ebcbffe852ad888a
Yuri Nakashima <yuri310@gmail.com>
2016-08-25 10:35:05 -0700

    completed class Stack implementation

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,27 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
-  end
- 
-  def pop
+    @store << element
   end
 
-  def top
+  def pop #take out last element in and return it
+    return @store.pop
   end
- 
+
+  def top #return the last thing in, which is at the top
+    return @store.last
+  end
+
   def size
+    return @store.length
   end
 
-  def empty?
+  def empty? #return size is calling the method above, def size
+    return size == 0 #@store.empty?, @store.length == 0
   end
 end
+
+#the rules of how you interact with array have been changed. the usual Array
+#methods will no longer work
