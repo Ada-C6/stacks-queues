a21d65bb123e6f9a890fbb91c3c44370da6a63d3
Alyssa Hursh <akhursh@gmail.com>
2016-08-25 10:45:09 -0700

    Hmmmm? I thought this committed.

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,24 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
+    @store << element # or @store.push(element)
   end
- 
+
   def pop
+    @store.pop
   end
 
   def top
+    @store.last
   end
- 
+
   def size
+    @store.length
   end
 
   def empty?
+    @store.empty?
   end
 end
