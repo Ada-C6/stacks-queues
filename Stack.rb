e0403efb0bf2b07140347a780b85c3578748dc6b
Natasha Ewing <natasharai@me.com>
2016-08-25 10:35:20 -0700

    completed stack init

--- Stack.rb
+++ Stack.rb
@@ -1,20 +1,31 @@
 class Stack
   def initialize
     @store = Array.new
+    #use array as underlying structure for the stack
+    #in this way, the stack is actually put into an array with rules placed on it conforming to the stack
   end
-  
+
   def push(element)
+    @store.push(element)
+    #the element goes to the end of the array
   end
- 
+
   def pop
+    return @store.pop
+    #automatically pops the last element off
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == [ ]
+    #@store.length == 0
+    #@store.empty?
   end
 end
