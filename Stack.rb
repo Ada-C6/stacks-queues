231c72f7f4b52e84f5341f2ba56fbb2d70694224
Carmen Chan <carmenkellychan@gmail.com>
2016-08-25 10:36:10 -0700

    Created a Stack class

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,25 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
+    @store.push(element)
   end
- 
+
   def pop
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return @store.empty?
+    #or size == 0 (calling the method size above)
   end
 end
