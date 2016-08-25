937c872851ff8eac4da0353b26a50de8559ba67c
Yuri Nakashima <yuri310@gmail.com>
2016-08-25 10:46:33 -0700

    made class Queue

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,27 @@
 class Queue
   def initialize
+    @store = []
   end
-  
+
   def enqueue(element)
+    @store << element
   end
- 
-  def dequeue
+
+  def dequeue #remove element from the front
+    return @store.shift
+    #or leave pop the same as stack, and put stuff into the front above
+    #@store.unshift(elemen)
   end
 
   def front
+    return @store.first
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0 #size called from above
   end
 end
