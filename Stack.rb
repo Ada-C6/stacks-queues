9ae5314acdfc6576105ac3a9e776eab131e9cbae
Esther Ng <esther.hc.ng@gmail.com>
2016-08-25 10:34:57 -0700

    Completed implementation of Stack class

--- Stack.rb
+++ Stack.rb
@@ -1,20 +1,25 @@
 class Stack
   def initialize
-    @store = Array.new
+    @store = Array.new # underlying structure
   end
-  
+
   def push(element)
+    @store << element
   end
- 
+
   def pop
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0 # @store.empty?  
   end
 end
