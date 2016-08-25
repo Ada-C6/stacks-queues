b1a95168ac691a1c606088dfb8bfef94aede7bd4
LaurenSky <ljherrer@gmail.com>
2016-08-25 10:35:39 -0700

    Completed creation of Stack class

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,24 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
+    @store.push(element)   #@store << element
   end
- 
+
   def pop
+    return @store.pop        #removes element at top and returns that element
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0    #@store.empty?    OR    #@store.length == 0
   end
 end
