f63e3c3bde27d48303c553fa53cfee6e4ddbecb3
Alyssa Hursh <akhursh@gmail.com>
2016-08-25 10:44:26 -0700

    Completed implementation of Queue class

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,25 @@
 class Queue
   def initialize
+    @store = Array.new
   end
-  
+
   def enqueue(element)
+    @store << element
   end
- 
+
   def dequeue
+    @store.shift
   end
 
   def front
+    @store.first
   end
- 
+
   def size
+    @store.length
   end
 
   def empty?
+    @store.empty?
   end
 end
