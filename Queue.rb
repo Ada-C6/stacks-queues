b8b1520f973dfcc9cc94c88e101fed92ed518b4e
Miriam Cortes <mcortes.umiami@gmail.com>
2016-08-25 10:43:49 -0700

    completed implementation of Queue

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,26 @@
 class Queue
   def initialize
+    @queue = []
   end
-  
+
   def enqueue(element)
+    @queue << element
+    # or @queue.push(element)
   end
- 
+
   def dequeue
+    return @queue.shift
   end
 
   def front
+    return @queue.first
   end
- 
+
   def size
+    return @queue.length
   end
 
   def empty?
+    return size == 0 # or @queue.empty?
   end
 end
