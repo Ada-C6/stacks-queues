7447526b05b74f9978b91bda98533311a4612004
Kate Shaffer <kate.shaffer@gmail.com>
2016-08-25 10:35:03 -0700

    Completed implementation of stack

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,26 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
+    @store.push(element)
+    #@store << element
   end
- 
+
   def pop
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    # return @store.empty?
+    return size == 0
   end
 end
