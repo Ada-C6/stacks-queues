fce641aebaab7a4955372ce2fb72b586d1ab8787
Johna N. Morris <johna.n.morris@gmail.com>
2016-08-25 10:27:38 -0700

    Implemented methods for Stack class

--- Stack.rb
+++ Stack.rb
@@ -1,20 +1,26 @@
 class Stack
-  def initialize
-    @store = Array.new
-  end
-  
-  def push(element)
-  end
- 
-  def pop
-  end
+    def initialize
+        @store = Array.new
+    end
 
-  def top
-  end
- 
-  def size
-  end
+    def push(element)
+        @store.push(element) # @store << element
+    end
 
-  def empty?
-  end
+    def pop
+        return @store.pop
+    end
+
+    def top
+        return @store.last
+    end
+
+    def size
+        return @store.length
+    end
+
+    def empty?
+        # calls size method
+        return size == 0 # store.empty?
+    end
 end
