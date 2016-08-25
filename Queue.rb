6a981da2b8cdd0c8a0da90fbe2dafe6d35514c0a
Melissa Rodriguez <olenellina@gmail.com>
2016-08-25 10:43:43 -0700

    adding completed implementation of Ruby Queue class

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,30 @@
 class Queue
   def initialize
+    @store = Array.new
   end
-  
+
   def enqueue(element)
+    @store.push(element)
   end
- 
+
   def dequeue
+    # Test this:
+    # x = @store[0]
+    # @store[0].delete
+    # return x
+    return @store.shift
   end
 
   def front
+    # return @store[0]
+    return @store.first
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return @store.length == 0
   end
 end
