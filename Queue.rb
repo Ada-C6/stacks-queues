277163ffe492ba7ceb2949c541f31b1f6c15d082
Yeni <yenicapote08@gmail.com>
2016-08-25 10:43:48 -0700

    completed implementation of queue

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,28 @@
 class Queue
   def initialize
+    @line = Array.new
   end
-  
+
   def enqueue(element)
+    @line.push(element)
+    #@store.unshift(element)
   end
- 
+
   def dequeue
+    return @line.shift
+    #return @store.pop
   end
 
   def front
+    return @line.first
+    #return @store.last
   end
- 
+
   def size
+    return @line.length
   end
 
   def empty?
+    size == 0
   end
 end
