3835e9bedd52e5a1f62db6de1713d3a06d4b41e0
heatherherrington <redherring@gmail.com>
2016-08-25 10:34:56 -0700

    Completed implementation of stack

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,26 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
-  def push(element)
+
+  def push(element) # Putting it into the array
+    @store.push(element) # or @store << element
   end
- 
+
   def pop
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0 # "size" calls size method, can also do
+    # @store.empty? OR
+    # @store.length == 0
   end
 end
