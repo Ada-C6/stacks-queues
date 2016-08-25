642a83f27a84da0abfde70979d2fcfc8803b9690
Nicole Url <nicole.url.4@gmail.com>
2016-08-25 10:34:55 -0700

    completed stack.rb implementation

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,25 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
+    @store.push(element)      # same as @store << element
+
   end
- 
+
   def pop
+    return @store.pop     # .pop works naturally as a stack pop (but not a a queue pop)
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
   end
 end
