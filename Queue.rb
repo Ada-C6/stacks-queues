1ba7da3f6649be660a86ae4d2793dfc946e91a51
Beylul <bkbreab8@gmail.com>
2016-08-25 10:49:42 -0700

    Completed Queue of  Implementation

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,27 @@
 class Queue
   def initialize
+    @store = Array.new
   end
-  
+
   def enqueue(element)
+    @store.push(element)
+    #or @store.unshift(element)
   end
- 
-  def dequeue
+
+  def dequeues
+    return @store.shift
+    #or @store.pop
   end
 
   def front
+    return @store.first
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
   end
 end
