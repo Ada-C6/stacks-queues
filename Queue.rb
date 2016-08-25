c64c950b5ef79972d2e0d554c3966c498da072b1
Shari Meggs <smeggs1@gmail.com>
2016-08-25 10:44:05 -0700

    This is the finished version of queue with methods

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,27 @@
 class Queue
   def initialize
+    @store = Array.new
   end
-  
+
   def enqueue(element)
+    @store << element
+    #option 2: @store,unshift(element)
   end
- 
+
   def dequeue
+    return @store.shift
+    #option 2:return @store.pop, same as stack
   end
 
   def front
+    return @store.first
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return @store.empty?
   end
 end
