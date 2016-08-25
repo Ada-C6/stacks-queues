e9a6c5163ef968cabe02cf638f2a2bd82273abba
Esther Ng <esther.hc.ng@gmail.com>
2016-08-25 10:43:14 -0700

    Implemented Queue class

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,25 @@
 class Queue
   def initialize
+    @store = []
   end
-  
+
   def enqueue(element)
+    @store << element # @store.unshift(element)
   end
- 
+
   def dequeue
+    return @store.shift # @store.pop
   end
 
   def front
+    return @store.first # @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
   end
 end
