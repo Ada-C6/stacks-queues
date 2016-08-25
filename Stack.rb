881e2a70bb0a1baf5532322bf30850d96c95b922
Beylul <bkbreab8@gmail.com>
2016-08-25 10:35:35 -0700

    Completed implementation of Stack.

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,26 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
+    @store.push(element) # element going to end of the list
+    # @store << element
   end
- 
-  def pop
+
+  def pop() # this takes one thing out
+  return @store.pop
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0 # or  @store.empty? # @store == 0
+    #size here is calling the method size.  .empthy is a method.
   end
 end
