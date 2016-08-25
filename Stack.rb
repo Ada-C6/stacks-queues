2f57fef3654fce404d03a4fa784fcc32d432e54d
Melissa Rodriguez <olenellina@gmail.com>
2016-08-25 10:34:56 -0700

    adding completed implementation of Ruby Stack class.

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,30 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
+      @store.push(element)
   end
- 
+
   def pop
+    # Test this
+    # x = @store[@store.length - 1]
+    # @store[@store.length - 1].delete
+    # return x
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
+    # return @store.empty
+    # return @store.length == 0
   end
 end
