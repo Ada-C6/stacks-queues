f91cbeb896b5995f3c98117e2d7bf4021fe821bb
Qlwentt <qlwentt@gmail.com>
2016-08-25 10:35:24 -0700

    Added stack implementation

--- Stack.rb
+++ Stack.rb
@@ -4,17 +4,23 @@ class Stack
   end
   
   def push(element)
+    @store<<element
+    #or @store.push(element)
   end
  
   def pop
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
  
   def size
+    return @store.length
   end
 
   def empty?
+    return size==0 #@store.empty? or @store.length
   end
 end
