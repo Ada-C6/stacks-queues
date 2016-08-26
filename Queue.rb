a21d3f6e3e4d2f58c08477704baea72eefc2a5b9
yael fine <yaelittle@gmail.com>
2016-08-26 15:25:00 -0700

    Queue.rb added for the record

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,28 @@
 class Queue
   def initialize
+    @line = Array.new
   end
-  
+
   def enqueue(element)
+    @line.push(element)
+    # or @store.unshift(element)
   end
- 
+
   def dequeue
+    return @line.shift
+    # or @store.pop
   end
 
   def front
+    return @line.first
+    # or @line.last
   end
- 
+
   def size
+    return @line.length
   end
 
   def empty?
+    return @line.empty?
   end
 end
