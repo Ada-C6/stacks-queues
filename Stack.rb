de4efd1204cc70c0c1caa8836e9278654813a4da
Rachel <rachel.l.pavilanis@gmail.com>
2016-08-27 16:57:20 -0700

    wrote Stack.rb

--- Stack.rb
+++ Stack.rb
@@ -1,20 +1,26 @@
 class Stack
   def initialize
-    @store = Array.new
+    @store = Array.new  #an array is an underlying structure for a stack
   end
-  
+
   def push(element)
+    @store.push(element)
+    #@store << element - same thing
   end
- 
+
   def pop
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0 # returns true/false, could also do @store.empty? OR @store.length == 0
   end
 end
