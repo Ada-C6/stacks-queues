2e36bb6bec03c58bae3f4f3d876be564669ccc76
Rachel <rachel.l.pavilanis@gmail.com>
2016-08-28 00:21:01 -0700

    finished draft of program, with added comments.

--- job-simulation.rb
+++ job-simulation.rb
@@ -1,15 +1,3 @@
-# A company has six hiring positions with more people wanting jobs than the
-# number of available positions.  The company managers decide in order to give
-# more people an opportunity to make money; they will allow people to work in
-# three-month intervals. The first six people on the waiting list will be hired
-# in the order that they are on the waiting list.  The first six people will
-# keep these positions for three months.  At the end of three months, the
-# manager will roll a dice to determine the number of people who will lose their
-# jobs. The company will use the policy of last-hired-first-fired.  For example,
-# if the dice roll is 3, the last 3 people hired will lose their jobs to the
-# first 3 people on the waiting list. People losing their jobs will be placed on
-# the back of the waiting list in the order that they are fired. This process
-# will continue for every three-month interval.
 
 require './Stack.rb'
 require './Queue.rb'
@@ -21,12 +9,14 @@ class CrappyCompany
 
 attr_accessor :currently_working, :waiting_list, :roll, :showing, :number
 
+# company starts with a waiting list, and then completes an initial hiring, taking the first 6 people from waitlist
   def initialize
     @waiting_list = ["Emma Woodhouse", "Emma Bovary", "Katniss Everdeen", "Lady Macbeth", "Anna Karenina", "Scout Finch", "Hester Prynne", "Luna Lovegood", "Liz Bennett", "Anne Elliot", "Hermione Granger", "Jane Eyre", "Arya Stark", "Jo March"]  # queue
     @people_currently_working = [] # stack
     initial_hiring
   end
 
+# initial hiring method, only called within initialize
   def initial_hiring
     6.times do
       @people_currently_working << @waiting_list.shift
@@ -34,29 +24,34 @@ attr_accessor :currently_working, :waiting_list, :roll, :showing, :number
     ap @people_currently_working
   end
 
-  def hiring
-    @number.times do
-      @people_currently_working << @waiting_list.shift
-    end
-    return @people_currently_working
-  end
-
+# process for firing, treats people_currently_working as a stack (the last people hired are the first fired)
   def firing
-    @number = time_passing
+    @number = time_passing # calls on time_passing method to roll die and determine number of people to fire
     @number.times do
       @waiting_list << @people_currently_working.pop
     end
     return @waiting_list
   end
 
+# process for hiring, treats waiting_list as a queue (the first people on waitlist are the first to begin working)
+  def hiring
+    @number.times do # uses number from roll in firing method
+      @people_currently_working << @waiting_list.shift
+    end
+    return @people_currently_working
+  end
+
+# artificially creates the idea of time passing - called each time the firing method is called
   def time_passing
     roll
   end
 
+# called by time_passing method - randomly returns a number from 1-6
   def roll
     return @showing = Random.rand(6) + 1  #instead of 0-5 inclusive, it becomes 1-6
   end
 
+# shows number on the die from roll (helpful in testing)
   def showing   #same as attr_reader -> just lets you see value
     return @showing
   end
@@ -64,9 +59,6 @@ attr_accessor :currently_working, :waiting_list, :roll, :showing, :number
 end
 
 company = CrappyCompany.new
-
-ap company.firing
-ap company.hiring
 ap company.firing
 ap company.hiring
 ap company.firing
