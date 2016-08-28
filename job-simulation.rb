df4df16b4afc6b57c9fa52544e250b4a3613eb10
Kate Shaffer <kate.shaffer@gmail.com>
2016-08-28 01:04:41 -0700

    Minor formatting/code cleanups

--- job-simulation.rb
+++ job-simulation.rb
@@ -10,49 +10,33 @@ first_applicants = ["Alana", "Bowser", "Cam", "Dalson", "Edgar", "Felix",
 
 bc = BadCompany.new
 bc.receive_applications(first_applicants)
+
 puts "_" * 80
-puts "BadCompany received #{ first_applicants.length.to_s } applications:\n\n"
+puts "
+  BadCompany received #{ first_applicants.length.to_s } applications:\n\n"
 puts bc.waiting_list.store
+
 puts "_" * 80
-puts "BadCompany hired 6 employees.\n\n"
+puts "
+  BadCompany hired 6 employees.\n\n"
 bc.hire(6)
+
 puts "_" * 80
 puts "
-The remaining waiting list is:\n\n"
+  The remaining waiting list is:\n\n"
 puts bc.waiting_list.store
-puts "_" * 80
-puts "
-3 months have passed. Time to fire by dice roll.\n\n"
-bc.fool_turnover
-puts "_" * 80
-puts "
-BadCompany employee list after firing/hiring:\n\n"
-puts bc.employee_list.store
-puts "_" * 80
-puts "
-BadCompany waiting list after firing/hiring:\n\n"
-puts bc.waiting_list.store
-puts "_" * 80
-puts "
-3 months have passed. Time to fire by dice roll.\n\n"
-bc.fool_turnover
-puts "_" * 80
-puts "
-BadCompany employee list after firing/hiring:\n\n"
-puts bc.employee_list.store
-puts "_" * 80
-puts "
-BadCompany waiting list after firing/hiring:\n\n"
-puts bc.waiting_list.store
-puts "_" * 80
-puts "
-3 months have passed. Time to fire by dice roll.\n\n"
-bc.fool_turnover
-puts "_" * 80
-puts "
-BadCompany employee list after firing/hiring:\n\n"
-puts bc.employee_list.store
-puts "_" * 80
-puts "
-BadCompany waiting list after firing/hiring:\n\n"
-puts bc.waiting_list.store
+
+3.times do
+  puts "_" * 80
+  puts "
+  3 months have passed. Time to fire by dice roll.\n\n"
+  bc.fool_turnover
+  puts "_" * 80
+  puts "
+  BadCompany employee list after firing/hiring:\n\n"
+  puts bc.employee_list.store
+  puts "_" * 80
+  puts "
+  BadCompany waiting list after firing/hiring:\n\n"
+  puts bc.waiting_list.store
+end
