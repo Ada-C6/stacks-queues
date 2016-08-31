2f759388f5fd8e0fa39e0c2dec6058c943a05c9a
Shari Meggs <smeggs1@gmail.com>
2016-08-30 19:54:16 -0700

    Program is functional and able to print out the changes.

--- job-simulation.rb
+++ job-simulation.rb
@@ -11,5 +11,51 @@
 # the back of the waiting list in the order that they are fired. This process
 # will continue for every three-month interval.
 
-require './Stack.rb'
-require './Queue.rb'
+require_relative './Stack.rb'
+require_relative './Queue.rb'
+class Job
+
+#The two groups are initialized here
+def initialize
+  @employed = Stack.new
+  @temp = Queue.new
+
+end
+
+#This helps to set the groups
+def set_places(arry_a, arry_b)
+arry_a.each do |i|
+  @employed.push(i)
+end
+arry_b.each do |i|
+  @temp.enqueue(i)
+end
+  return @employed, @temp
+end
+
+# Roll dice method sets the amount of people hired and fired and moves them accordingly
+# The respective groups are added to the end of the other group
+def roll_dice
+  fired_peeps = " "
+  new_hires = " "
+  dice = rand(1..5) + 1
+  puts "Boss rolled #{dice}:"
+  puts "List of fired \t\t List of Hired"
+  dice.times do
+  fired_peeps = @employed.pop
+  new_hires = @temp.dequeue
+  puts "#{fired_peeps} \t\t\t\t #{new_hires}"
+  @employed.push(new_hires)
+  @temp.enqueue(fired_peeps)
+end
+end
+
+end
+
+iit = Job.new_hires
+lucky_people = %w(a b c d e f)
+other_folks  = %w(g h i j k l m n o p q r s)
+iit.set_places(lucky_people, other_folks)
+iit.roll_dice
+iit.roll_dice
+iit.roll_dice
